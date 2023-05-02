1867bcd8ef976b94e139ff1635217d96f0c6daf8
Beto Toro <beto.toro@zemoga.com>
2023-05-02 16:51:52 -0500

    config(linter): add and try to adust the ESlint and Prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    './node_modules/standard/eslintrc.json'
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
+    'space-before-function-paren': 'off',
+    'jsx-quotes': 'off'
+  }
 }
